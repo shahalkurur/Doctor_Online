294d8479071132ec9cc5a42a6ffe81f4afe55fd1
shahalkurur <shahalk1234@gmail.com>
2024-12-07 17:49:44 +0530

    first commit

---
+++ Back_end/doctor_online/home/serializers.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class CreateUser(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields=['id','username','email','password']
+        extra_kwargs={'password':{'write_only':True}}
+    def create(self,validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
